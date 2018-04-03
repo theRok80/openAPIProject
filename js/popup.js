ff6d1d3f879d4a4b724cac8773c8722550d1847a
LEE JUNG ROK <therok80@gmail.com>
2018-04-03 19:19:15 +0900

    commit

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,46 @@
-let changeColor = document.getElementById('changeColor');
-let restoreColor = document.getElementById('restoreColor');
+var sayThis;
+var wordList;
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+$(document).ready(function() {
+    sayThis = $("#sayThis");
+    wordList = $("#wordList");
+
+    // chrome.tts.getVoices(
+    //     function(voices) {
+    //       for (var i = 0; i < voices.length; i++) {
+    //         console.log('Voice ' + i + ':');
+    //         console.log('  name: ' + voices[i].voiceName);
+    //         console.log('  lang: ' + voices[i].lang);
+    //         console.log('  gender: ' + voices[i].gender);
+    //         console.log('  extension id: ' + voices[i].extensionId);
+    //         console.log('  event types: ' + voices[i].eventTypes);
+    //       }
+    //     });
+
+    chrome.tabs.executeScript( {
+        code: "window.getSelection().toString();"
+    }, function(selection) {
+        var string = selection[0];
+        if (string.length > 0) {
+            sayThis.val(string);
+        }
+    });
+
+    $('#speak').click(function(event){
+        chrome.tts.stop();
+        if (sayThis.val()) {
+            chrome.tts.speak(sayThis.val(), {
+                rate : 0.8,
+                lang: "ko-KR",
+                voiceName: "Google 한국의",
+                onEvent: function(event) {
+                    // console.log(event.type);
+                }
+            });
+        }
+    });
+
+    sayThis.focus(function(event){
+        chrome.tts.stop();
+    });
 });
-
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.executeScript(
-        // tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'}
-    );
-  };
-
-restoreColor.onclick = function() {
-    chrome.tabs.executeScript(
-        {code: 'document.body.style.backgroundColor = null;'}
-    );
-};
